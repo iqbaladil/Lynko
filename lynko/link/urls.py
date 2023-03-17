6e2eae5e182c720079bb684f0732aa8f4d4bed6f
Adil <iqbal.adil88@gmail.com>
2023-03-17 14:11:54 +0500

    Edit and delete categories & links

--- lynko/link/urls.py
+++ lynko/link/urls.py
@@ -5,6 +5,11 @@ app_name = 'links'
 
 urlpatterns = [
     path('', views.links, name='links'),
-    path('create-category/', views.create_category, name='create_category'),
+    path('<int:pk>/edit/', views.edit_link, name='edit_link'),
+    path('<int:pk>/delete/', views.delete_link, name='delete_link'),
+    path('categories/', views.categories, name='categories'),
+    path('categories/create-category/', views.create_category, name='create_category'),
+    path('categories/<int:pk>/edit/', views.edit_category, name='edit_category'),
+    path('categories/<int:pk>/delete/', views.delete_category, name='delete_category'),
     path('create-link/', views.create_link, name='create_link'),
 ]
--- lynko/link/views.py
+++ lynko/link/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CategoryForm, LinkForm
 from .models import Category, Link
 
@@ -10,6 +10,18 @@ def links(request):
     return render(request, 'link/links.html', {
         'links': links,
     })
+
+
+
+@login_required
+def categories(request):
+    categories = Category.objects.filter(created_by=request.user)
+
+    return render(request, 'link/categories.html', {
+        'categories': categories
+    })
+
+
 
 @login_required
 def create_link(request):
@@ -29,9 +41,40 @@ def create_link(request):
         form.fields['category'].queryset = Category.objects.filter(created_by=request.user)
 
     return render(request, 'link/create_link.html', {
-        'form': form
+        'form': form,
+        'title':'Create Link',
     })
 
+
+@login_required
+def edit_link(request, pk):
+    link = get_object_or_404(Link, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = LinkForm(request.POST, instance=link)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/links/')
+    else:
+        form = LinkForm(instance=link)
+
+        # Show only specific login user's categories in dropdown
+        form.fields['category'].queryset = Category.objects.filter(created_by=request.user)
+
+    return render(request, 'link/create_link.html', {
+        'form': form,
+        'title':'Edit Link',
+    })
+
+
+@login_required
+def delete_link(request, pk):
+    link = get_object_or_404(Link, created_by=request.user, pk=pk)
+    link.delete()
+
+    return redirect('/links/')
 
 
 # Create category will only be available if the user is login.
@@ -50,5 +93,36 @@ def create_category(request):
         form = CategoryForm()
 
     return render(request, 'link/create_category.html', {
-        'form': form
+        'form': form,
+        'title': 'Create Category',
     })
+
+
+def edit_category(request, pk):
+    # get_object_494() package is a shortcut to get things into the database.
+    category = get_object_or_404(Category, created_by=request.user, pk=pk)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/links/categories/')
+    else:
+        # instance is use so that all the fields are automatically fill out in form.
+        form = CategoryForm(instance=category)
+
+    return render(request, 'link/create_category.html', {
+        'form': form,
+        'title': 'Edit Category',
+    })
+
+
+@login_required
+def delete_category(request, pk):
+    # get_object_494() package is a shortcut to get things into the database.
+    category = get_object_or_404(Category, created_by=request.user, pk=pk)
+    category.delete()
+
+    return redirect('/links/categories/')
